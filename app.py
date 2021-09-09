d619c4cceef0a3661c67ee9cf1f05972d3b443f7
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-09-09 18:23:07 +0100

    Create the Flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, flash, render_template, request, session, url_for, redirect
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from pymongo.message import query
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# imports environment variables if path to "env.py" is found
+if os.path.exists("env.py"):
+    import env
+
+# creates instance of Flask app
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+# Tells App where to run
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
